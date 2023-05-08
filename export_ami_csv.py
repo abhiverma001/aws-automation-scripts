855924a2b1e6082a8c6f8f8a1facc2ec9af1bb17
Abhishek Verma <94762544+abhiverma001@users.noreply.github.com>
2023-05-08 23:53:47 +0530

    Add files via upload

---
+++ export_ami_csv.py
@@ -0,0 +1,20 @@
+import boto3
+import csv
+
+client = boto3.client('ec2')
+
+# Retrieve all available AMIs
+response = client.describe_images(Owners=['self'])
+
+# Extract the relevant details and write to CSV
+with open('ami_details.csv', mode='w', newline='') as csv_file:
+    fieldnames = ['ImageId', 'Name', 'CreationDate', 'VirtualizationType', 'State']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for image in response['Images']:
+        writer.writerow({'ImageId': image['ImageId'], 
+                         'Name': image['Name'], 
+                         'CreationDate': image['CreationDate'], 
+                         'VirtualizationType': image['VirtualizationType'], 
+                         'State': image['State']})
